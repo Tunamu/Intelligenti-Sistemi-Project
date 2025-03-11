5e34046ae81cc3baf6b22ad43c9207d821db0cd4
Tunamu <tunamuswork@gmail.com>
2025-03-11 14:52:41 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
 import numpy as np 
-import cv2 as sv
+import cv2
 from PIL import Image
 
 import pytesseract
 
-image = cv.imread('./data/IMG_5239.jpg')
+image = cv2.imread('./data/IMG_5239.jpg')
 assert image is not None ,"file could not be read, check with os.path.exists()"
-ball = img[280:340, 330:390]
+ball = image[280:340, 330:390]
 print(ball)
-#print(pytesseract.image_to_string(Image.open('IMG_5239.jpg')))
+print(pytesseract.image_to_string(Image.open('IMG_5239.jpg')))
 #My Datalist : IMG_5239.jpg - IMG_5254.jpg
-
