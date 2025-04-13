1961fb746909a8d8057c4d5a79eddf337c69c9c8
Tunamu <tunamuswork@gmail.com>
2025-04-13 02:14:29 +0200

    added little csv with random forest method

--- Scripts/Assignment5-RandomForest.py
+++ Scripts/Assignment5-RandomForest.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 
-file = pd.read_csv("../big_pixel_counts_new/big_pixel_counts_new_10.csv")
+file = pd.read_csv("../little_pixel_counts_new/little_pixel_counts_new_5.csv")
 X = file.drop(columns=["Image","Expected_Letter", "Ocp_Letter"]).values
 y = file["Expected_Letter"].values
 le = LabelEncoder()
@@ -17,5 +17,5 @@ rf_pred = rf_model.predict(X)
 rf_pred_transformed = le.inverse_transform(rf_pred)
 
 file["Letter_From_Random_Forest"] = rf_pred_transformed
-file.to_csv("final_datasets/final_dataset_with_random_forest_10.csv", index=False)
+file.to_csv("../final_datasets/final_dataset_with_random_forest_little_5.csv", index=False)
 print("Script Completed")
